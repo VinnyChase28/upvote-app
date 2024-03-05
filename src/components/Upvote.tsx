2ba4fd4fb87671f87f4ae5c565a01a06a0dc69ec
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-05 12:19:13 -0800

    add placeholder components

---
+++ src/components/Upvote.tsx
@@ -0,0 +1,13 @@
+//create placeholder for upvote component
+
+import React from "react";
+
+const Upvote = () => {
+  return (
+    <div>
+      <button>Upvote</button>
+    </div>
+  );
+};
+
+export default Upvote;
---
+++ src/components/UpvoteList.tsx
@@ -0,0 +1,13 @@
+//create placeholder for upvote component
+
+import React from "react";
+
+const UpvoteList = () => {
+  return (
+    <div>
+      <button>Upvote</button>
+    </div>
+  );
+};
+
+export default UpvoteList;
