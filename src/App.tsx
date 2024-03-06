afa7cbed06e55d3a1ff7535ef61ecf9296485d01
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-05 21:11:01 -0800

    add notes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1 className="text-xl p-6">All functionality persists.</h1>
       <header className="App-header">
         <div className="flex justify-center p-4">
           <div className="max-w-2xl w-full">
             <ArrowList />
           </div>
         </div>
+        <h1 className="text-xl p-6">Extras</h1>
         <div className="flex justify-center items-center p-4">
           <div className="flex flex-col md:flex-row gap-8 justify-between items-start w-full max-w-6xl">
             <div className="w-full md:w-1/2 p-4 bg-white rounded-lg shadow-md">
